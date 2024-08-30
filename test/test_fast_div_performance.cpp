6f3c6ffc0e4ba250696d43e2b08f181765c9455e
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-08-30 08:20:18 -0500

    Unit test: speed up & simplify perf tests

--- test/test_fast_div_performance.cpp
+++ test/test_fast_div_performance.cpp
@@ -4,45 +4,21 @@
 #include "lambda_timer.hpp"
 #include "unity_print_timers.hpp"
 #include "test_utils.h"
-
-// Performance test divisors are created by iterating over a fixed integer range with a fixed step.
-// E.g. [0, UINT16_MAX], step size 7777U
-// The generateDividendFromDivisor family of functions maps the divisor to a dividend by interpolation.
-template <typename TDivisor>
-static uint16_t generateDividendFromDivisorU12(TDivisor index, TDivisor min, TDivisor max) {
-  return generateDividendFromDivisorU<TDivisor, uint16_t>(index, min, max, 12U);
-}
-template <typename TDivisor>
-static uint16_t generateDividendFromDivisorU16(TDivisor index, TDivisor min, TDivisor max) {
-  return generateDividendFromDivisorU<TDivisor, uint16_t>(index, min, max, 16U);
-}
-template <typename TDivisor>
-static uint32_t generateDividendFromDivisorU24(TDivisor index, TDivisor min, TDivisor max) {
-  return generateDividendFromDivisorU<TDivisor, uint32_t>(index, min, max, 24U);
-}
-template <typename TDivisor>
-static uint32_t generateDividendFromDivisorU32(TDivisor index, TDivisor min, TDivisor max) {
-  return generateDividendFromDivisorU<TDivisor, uint32_t>(index, min, max, 32U);
-}
-
-template <typename TDivisor>
-static int16_t generateDividendFromDivisorI16(TDivisor index, TDivisor min, TDivisor max) {
-  return interpolate<TDivisor, int16_t>(index, min, max, INT16_MIN, INT16_MAX);
-}
-template <typename TDivisor>
-static int32_t generateDividendFromDivisorI32(TDivisor index, TDivisor min, TDivisor max) {
-  return interpolate<TDivisor, int32_t>(index, min, max, INT32_MIN, INT32_MAX);
-}
+#include "index_range_generator.hpp"
 
 static void test_fast_div_u16_u8_perf(void)
 {
-  constexpr uint8_t start_index = 1U;
-  constexpr uint8_t end_index = UINT8_MAX;
-  constexpr uint8_t step = 1;
+  constexpr uint16_t steps = UINT8_MAX-1U;
+  static constexpr index_range_generator<uint8_t> divisorGen(1U, UINT8_MAX, steps);
+  static constexpr index_range_generator<uint16_t> dividendGen(1U, UINT16_MAX, steps);
 
-  auto nativeTest = [] (uint8_t index, uint32_t &checkSum) { checkSum += (uint32_t)(generateDividendFromDivisorU16(index, start_index, end_index) / (uint16_t)index); };
-  auto optimizedTest = [] (uint8_t index, uint32_t &checkSum) { checkSum += (uint32_t)fast_div(generateDividendFromDivisorU16(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<uint8_t, uint32_t>(20, start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint8_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint8_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint8_t, uint32_t>(32U, 0U, steps, 1U, nativeTest, optimizedTest);
   
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
@@ -54,13 +30,17 @@ static void test_fast_div_u16_u8_perf(void)
 
 static void test_fast_div_u16_u16_perf(void)
 {
-  constexpr uint16_t step = UINT16_MAX/7777U;
-  constexpr uint16_t start_index = 1U;
-  constexpr uint16_t end_index = UINT16_MAX - step;
+  constexpr uint16_t steps = 3333U;
+  static constexpr index_range_generator<uint16_t> divisorGen(1U, UINT16_MAX/2U, steps);
+  static constexpr index_range_generator<uint16_t> dividendGen((UINT16_MAX/2U)+1U, UINT16_MAX, steps);
 
-  auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += (uint32_t)((uint16_t)generateDividendFromDivisorU16(index, start_index, end_index) / (uint16_t)index); };
-  auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += (uint32_t)fast_div((uint16_t)generateDividendFromDivisorU16(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<uint16_t, uint32_t>(start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint16_t, uint32_t>(0U, steps, 1U, nativeTest, optimizedTest);
   
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
@@ -75,13 +55,17 @@ static void test_fast_div_u16_u16_perf(void)
 
 static void test_fast_div_u24_u16_perf(void)
 {
-  constexpr uint16_t step = UINT16_MAX/7777U;
-  constexpr uint16_t start_index = 1U;
-  constexpr uint16_t end_index = UINT16_MAX - step;
+  constexpr uint16_t steps = 3333U;
+  static constexpr index_range_generator<uint16_t> divisorGen(1U, UINT16_MAX, steps);
+  static constexpr index_range_generator<uint32_t> dividendGen(1UL, 1UL<<24UL, steps);
 
-  auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += generateDividendFromDivisorU24(index, start_index, end_index) / (uint32_t)index; };
-  auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += fast_div(generateDividendFromDivisorU24(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<uint16_t, uint32_t>(start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint16_t, uint32_t>(0U, steps, 1U, nativeTest, optimizedTest);
 
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
@@ -93,13 +77,17 @@ static void test_fast_div_u24_u16_perf(void)
 
 static void test_fast_div_u32_u16_perf(void)
 {
-  constexpr uint16_t step = UINT16_MAX/5355U;
-  constexpr uint16_t start_index = 1;
-  constexpr uint16_t end_index = UINT16_MAX - step;
+  constexpr uint16_t steps = 3333U;
+  static constexpr index_range_generator<uint16_t> divisorGen(1U, UINT16_MAX, steps);
+  static constexpr index_range_generator<uint32_t> dividendGen(1UL, UINT32_MAX, steps);
 
-  auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += generateDividendFromDivisorU32(index, start_index, end_index) / (uint32_t)index; };
-  auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) { checkSum += fast_div(generateDividendFromDivisorU32(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<uint16_t, uint32_t>(start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint16_t, uint32_t>(0U, steps, 1U, nativeTest, optimizedTest);
 
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
@@ -114,13 +102,17 @@ static void test_fast_div_u32_u16_perf(void)
 
 static void test_fast_div_u32_u32_perf(void)
 {
-  constexpr uint32_t step = UINT32_MAX/7777U;
-  constexpr uint32_t start_index = 1;
-  constexpr uint32_t end_index = UINT32_MAX - step;
+  constexpr uint16_t steps = 3333U;
+  static constexpr index_range_generator<uint32_t> divisorGen(1UL, UINT32_MAX/2UL, steps);
+  static constexpr index_range_generator<uint32_t> dividendGen(UINT32_MAX/2UL+1, UINT32_MAX, steps);
 
-  auto nativeTest = [] (uint32_t index, uint32_t &checkSum) { checkSum += generateDividendFromDivisorU32(index, start_index, end_index) / (uint32_t)index; };
-  auto optimizedTest = [] (uint32_t index, uint32_t &checkSum) { checkSum += fast_div(generateDividendFromDivisorU32(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<uint32_t, uint32_t>(start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint16_t, uint32_t>(0U, steps, 1U, nativeTest, optimizedTest);
 
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
@@ -135,13 +127,17 @@ static void test_fast_div_u32_u32_perf(void)
 
 static void test_fast_div_s32_s16_perf(void)
 {
-  constexpr int16_t step = INT16_MAX/3333;
-  constexpr int16_t start_index = INT16_MIN;
-  constexpr int16_t end_index = INT16_MAX - step;
+  constexpr uint16_t steps = 3333U;
+  static constexpr index_range_generator<int16_t> divisorGen(INT16_MIN, INT16_MAX, steps);
+  static constexpr index_range_generator<int32_t> dividendGen(INT32_MIN, INT32_MAX, steps);
 
-  auto nativeTest = [] (int16_t index, int32_t &checkSum) { checkSum += generateDividendFromDivisorI32(index, start_index, end_index) / (int32_t)index; };
-  auto optimizedTest = [] (int16_t index, int32_t &checkSum) { checkSum += fast_div(generateDividendFromDivisorI32(index, start_index, end_index), index); };
-  auto comparison = compare_executiontime<int16_t, int32_t>(start_index, end_index, step, nativeTest, optimizedTest);
+  static auto nativeTest = [] (uint16_t index, uint32_t &checkSum) { 
+    checkSum += dividendGen.generate(index) / divisorGen.generate(index);
+  };
+  static auto optimizedTest = [] (uint16_t index, uint32_t &checkSum) {
+    checkSum += fast_div(dividendGen.generate(index), divisorGen.generate(index));
+  };
+  auto comparison = compare_executiontime<uint16_t, uint32_t>(0U, steps, 1U, nativeTest, optimizedTest);  
 
   MESSAGE_TIMERS(comparison.timeA.timer, comparison.timeB.timer);
   TEST_ASSERT_EQUAL(comparison.timeA.result, comparison.timeB.result);
--- test/test_utils.h
+++ test/test_utils.h
@@ -25,28 +25,3 @@ static inline void ufname_szrestore(char** __s)
 for ( UNITY_FILENAME_RESTORE, _ufname_done = ufname_set(__FILE__);                  \
     _ufname_done; _ufname_done = 0 )
 
-// ============================ Overflow free interpolation ============================ 
-
-template <typename TIn, typename TOut>
-static inline constexpr float compute_slope(const TIn &inMin, const TIn &inMax, const TOut &outMin, const TOut &outMax) {
-  return (float)(outMax-outMin) / ((float)inMax-(float)inMin);
-}
-
-// Interpolate avoiding integer overflow.
-// Very slow - do not use in production code.
-template <typename TIn, typename TOut>
-static inline constexpr TOut interpolate(const TIn x, const TIn inMin, const TIn inMax, const TOut outMin, const TOut outMax) {
-  // We use floating point intermediate to avoid integer overflow
-  return (TOut)(compute_slope(inMin, inMax,outMin, outMax) * ((float)x-(float)inMin));
-}
-
-// ============================ Dividend generation ============================ 
-
-// Performance test divisors are created by iterating over a fixed integer range with a fixed step.
-// E.g. [0, UINT16_MAX], step size 7777U
-// The function functions map an unsigned divisor to a unsigned dividend by interpolation, limiting the
-// upper bound of the dividend range by bit width
-template <typename TDivisor, typename TDividend>
-static inline TDividend generateDividendFromDivisorU(TDivisor index, TDivisor min, TDivisor max, uint8_t bitRangeWidth) {
-  return interpolate<TDivisor, TDividend>(index, min, max, 1U, (TDividend)1U<<(TDividend)bitRangeWidth);
-}
